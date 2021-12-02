6468171d9dbc5ebcfca5c70ab795ee64f8222e93
Armin Purle-Kopacz <pukoarmin@gmail.com>
2021-12-02 22:22:29 +0200

    Update parser.py

--- Lab 6/model/parser.py
+++ Lab 6/model/parser.py
@@ -4,41 +4,68 @@ def head(stack):
     except:
         pass
 
-
 class Parser:
     def __init__(self, grammar):
         self.grammar = grammar
-        self.states = {"normal_state": "normal",
-                       "back_state": "back_state",
-                       "final_state": "final_state",
-                       "error_state": "error"}
-        self.s = self.states["normal_state"]
-        self.i = 1
+
+class ParserConfig:
+    def __init__(self, grammar):
+        self.grammar = grammar
+        self.s = 'q'
+        self.i = 0
         self.alpha = []
-        self.beta = [grammar.starting_symbol]
+        self.beta = []
 
     def __expand(self):
-        pass
+        current = self.beta.pop()
+        production = self.grammar.get_productions_for_non_terminal(current)
+        if production.rules[0] != "epsilon":
+            self.beta += reversed(production.rules[0])
+        self.beta.append(production.rules[0] + "#0")
 
     def __advance(self):
-        pass
+        self.i += 1
+        terminal = self.beta.pop()
+        self.alpha.append(terminal)
 
     def __momentary_insuccess(self):
-        pass
+        self.s = 'b'
 
     def __back(self):
-        pass
+        self.i -= 1
+        terminal = self.beta.pop()
+        self.alpha.append(terminal)
 
     def __another_try(self):
-        pass
+        symbl = self.alpha.pop()
+        non_terminal, production_nbr = symbl.split("#")
+        production_nbr = int(production_nbr)
+        productions = self.grammar.get_productions_for_non_terminal(non_terminal)
+
+        current_production = productions[production_nbr]
+        for el in current_production:
+            el = self.beta.pop()
+
+        if production_nbr < len(productions) - 1:
+            new_production = productions[production_nbr + 1]
+            if new_production != "epsilon":
+                self.beta += reversed(new_production)
+            self.beta.append(non_terminal + "#{0}".format(production_nbr + 1))
+            self.s = 'q'
+            return
+
+        if self.i == 0 and non_terminal == self.grammar.S:
+            self.s = 'e'
+            return
+        self.beta.append(non_terminal)
 
     def __success(self):
-        pass
+        self.s = 'f'
 
     # Descendant Recursive
     def parse(self):
-        while self.s not in self.grammar.terminals and self.s != self.states["error_state"]:
-            if self.s == self.states["normals_state"]:
+        while self.s not in self.grammar.terminals and self.s != 'e':
+            if self.s == 'q':
                 if self.i == len(self.grammar.productions) and len(self.beta) == 0:
                     self.__success()
                 else:
@@ -49,13 +76,13 @@ class Parser:
                             self.__advance()
                         else:
                             self.__momentary_insuccess()
-            if self.s == self.states["back_state"]:
+            if self.s == 'b':
                 if head(self.alpha) = "a":
                     self.__back()
                 else:
                     self.__another_try()
 
-        if self.s == self.states["error_state"]:
+        if self.s == 'e':
             print("[PARSER] -> Error")
         else:
             print("[PARSER] -> Sequence accepted")
